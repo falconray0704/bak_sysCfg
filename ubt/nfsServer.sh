94c4387b55779b26eef9cd286fdfa9dafd968476
FalconRay <falconray@yahoo.com>
2019-01-13 13:38:04 +0800

    Refine nfs server deploy tips.

--- ubt/nfsServer.sh
+++ ubt/nfsServer.sh
@@ -12,7 +12,13 @@ sudo apt-get -y install nfs-kernel-server
 #sudo vim /etc/exports
 #/opt/ums    *(rw,sync,no_root_squash,no_subtree_check)
 sudo systemctl start nfs-kernel-server.service
+
 #//linux fstab mount: 
 #172.16.231.1:/mnt/ld0/github/falcon       /opt/github/falcon nfs defaults 0 0
+#172.16.231.1:/mnt/ld0/github/falcon       /opt/github/falcon nfs4 auto,noatime,nolock,bg,nfsvers=4,intr,tcp,actimeo=1800 0 0
+#172.16.231.1:/mnt/ld0/github/falcon       /opt/github/falcon nfs rw,sync,nolock,tcp,noauto,x-systemd.automount,x-systemd.device-timeout=30,_netdev,atime,actimeo=3600 0 2
+
+
+
 #//Mac mount: sudo mount -t nfs -o rw,resvport 172.16.197.180:/opt/ums ums
 
