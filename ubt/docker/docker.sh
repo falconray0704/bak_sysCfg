791cd453b7d3e0046f00a2170af95b3a8bcb1881
FalconRay <falconray@yahoo.com>
2019-01-13 16:58:39 +0800

    Deploy docker-compose 1.23.2 in ubuntu.

--- ubt/docker/docker.sh
+++ ubt/docker/docker.sh
@@ -8,11 +8,11 @@ set -x
 install_DockerCompose_func()
 {
     # refer to https://docs.docker.com/compose/install/#install-compose
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
     # https://docs.docker.com/compose/completion/#install-command-completion
-    sudo curl -L https://raw.githubusercontent.com/docker/compose/1.22.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+    sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 
     # check
     docker-compose --version
