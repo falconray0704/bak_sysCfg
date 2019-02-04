f62e94e432c8ea822248a262b12c723342284ed9
FalconRay <falconray@yahoo.com>
2019-02-04 14:07:51 +0800

    Refine tips for ubuntu.

--- ubt/tips.sh
+++ ubt/tips.sh
@@ -5,27 +5,39 @@ set -o errexit
 
 #set -x
 
+. ../libShell/echo_color.lib
+
 tips_compress_func()
 {
-    echo "[001] Compress file with multi CPU:"
+    echoC "[001] Compress file with multi CPU:"
     echo 'pbzip2 -kzvp4 <input file name'
     echo 'tar cf <file name>.tar.bz2 --use-compress-prog=pbzip2 <dir_to_compress>'
     echo ""
 }
 
-tips_help_func()
+tips_curl_func()
 {
-    echo "Supported tips:"
-    echo '001) [compress]        Tips for compression.'
+    echoC "Review curl request infos:"
+    echo "curl -v www.doryhub.com > /dev/null"
 }
 
+tips_help_func()
+{
+    echoC "Supported tips:"
+    echo '001) [compress]       Tips for compression.'
+    echo '002) [curl]           Tips for curl.'
+}
+
+[ $# -lt 1 ] && tips_help_func && exit
 
 case $1 in
-    compress) echo "001) [compression] Tips:"
+    compress) echoR "001) [compression] Tips for compreession:"
         tips_compress_func
         ;;
-    help|*) echo "Tips for ubuntu:"
-        tips_help_func
+    curl) echoR "002) [curl] Tips for curl:"
+        tips_curl_func
+        ;;
+    *) echo "Unknown command:"
         ;;
 esac
 
