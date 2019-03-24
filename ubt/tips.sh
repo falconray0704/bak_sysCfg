af0618f3d6dae3646fa64ef3b555c295b7c4f05b
FalconRay <falconray@yahoo.com>
2019-03-24 13:03:04 +0800

    Refine tips of  modify system resource limitation.

--- ubt/tips.sh
+++ ubt/tips.sh
@@ -21,11 +21,64 @@ tips_curl_func()
     echo "curl -v www.doryhub.com > /dev/null"
 }
 
+tips_sysLimit_func()
+{
+    echoC "(1) Changing apply to system config file:"
+    echo ""
+    echo "Change limitation of file opening for all processes in the system:"
+    echo 'echo 2000500 > /proc/sys/fs/file-max'
+    echo "or"
+    echo 'sysctl -w "fs.file-max=2000500"'
+    echo ""
+    echo "Change resource limitation for current shell and processes were opened in it:"
+    echo '"echo 2000500 > /proc/sys/fs/nr_open" and "ulimit -n 2000500"'
+    echo "or"
+    echo "sysctl -w fs.nr_open=2000500"
+    echo ""
+    echo "Change limitation of connection by iptables:"
+    echo "sysctl -w net.nf_conntrack_max=2000500"
+    echo ""
+    echo "Check limitation of connection by iptables:"
+    echo "wc -l /proc/net/nf_conntrack"
+    echo "Check max limitation of connection by iptables:"
+    echo "cat /proc/sys/net/nf_conntrack_max"
+
+    echoC "(2) Changing apply to system temporary:"
+    echo ""
+    echo "Change limitation of file opening for all processes in the system:"
+    echo '"vim /etc/sysctl.conf" make "fs.file-max = 2000500" , and apply it by "sysctl -w"'
+    echo ""
+    echo "Change resource limitation for current shell and processes were opened in it:"
+    echo '"vim /etc/sysctl.conf" make "fs.nr_open = 2000500"'
+    echo "and"
+    echo 'vim "/etc/security/limits.conf" make "* soft nofile 2000500" and "* hard nofile 2000500"'
+    echo ""
+    echo "Change limitation of connection by iptables:"
+    echo '"vim /etc/sysctl.conf" make "net.nf_conntrack_max = 2000500"'
+
+    echoC "(3) Optimizations of net stack for testing:"
+    echo "sysctl -w fs.file-max=2000500"
+    echo "sysctl -w fs.nr_open=2000500"
+    echo "sysctl -w net.nf_conntrack_max=2000500"
+    echo "ulimit -n 2000500"
+    echo "sysctl -w net.ipv4.tcp_mem='131072  262144  524288'"
+    echo "sysctl -w net.ipv4.tcp_rmem='8760  256960  4088000'"
+    echo "sysctl -w net.ipv4.tcp_wmem='8760  256960  4088000'"
+    echo "sysctl -w net.core.rmem_max=16384"
+    echo "sysctl -w net.core.wmem_max=16384"
+    echo "sysctl -w net.core.somaxconn=2048"
+    echo "sysctl -w net.ipv4.tcp_max_syn_backlog=2048"
+    echo "sysctl -w /proc/sys/net/core/netdev_max_backlog=2048"
+    echo "sysctl -w net.ipv4.tcp_tw_recycle=1"
+    echo "sysctl -w net.ipv4.tcp_tw_reuse=1"
+}
+
 tips_help_func()
 {
     echoC "Supported tips:"
     echo '001) [compress]       Tips for compression.'
     echo '002) [curl]           Tips for curl.'
+    echo '003) [sysLimit]       Tips for change system limitations.'
 }
 
 [ $# -lt 1 ] && tips_help_func && exit
@@ -36,6 +89,9 @@ case $1 in
         ;;
     curl) echoR "002) [curl] Tips for curl:"
         tips_curl_func
+        ;;
+    sysLimit) echoR "002) [sysLimit] Tips for change system limitations:"
+        tips_sysLimit_func
         ;;
     *) echo "Unknown command:"
         ;;
