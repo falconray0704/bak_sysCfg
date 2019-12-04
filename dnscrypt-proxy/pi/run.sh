b7c690fa37b9851241856f5455f3efda6008bc91
FalconRay <falconray@yahoo.com>
2019-12-04 18:07:03 +0800

    Migrate dnscrypt-proxy deployment operations from dkhub repo.

--- dnscrypt-proxy/pi/run.sh
+++ dnscrypt-proxy/pi/run.sh
@@ -7,6 +7,9 @@ set -e
 
 ARCH=$(arch)
 
+RELEASE_ROOT_DIR="deployPkgs"
+INSTALL_ROOT_PATH=${HOME}/${RELEASE_ROOT_DIR}/${ARCH}
+
 check_dns_func()
 {
     ss -lp 'sport = :domain'
@@ -29,30 +32,76 @@ uninstall_systemDNS_func()
 
 }
 
+config_func()
+{
+
+    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+    cp example-dnscrypt-proxy.toml dnscrypt-proxy.toml
+	sed -i "s/^# server_names =.*/server_names = \['cisco', 'google', 'scaleway-fr', 'yandex', 'cloudflare'\]/" dnscrypt-proxy.toml
+	sed -i "s/^listen_addresses =.*/listen_addresses = \['127.0.0.1:53'\]/" dnscrypt-proxy.toml
+	sed -i "s/.*ignore_system_dns =.*/ignore_system_dns = true/" dnscrypt-proxy.toml
+	sed -i "s/.*force_tcp =.*/force_tcp = true/" dnscrypt-proxy.toml
+	sed -i "s/^timeout =.*/timeout = 3000/" dnscrypt-proxy.toml
+
+    echoY "Downloading public-resolvers..."
+    set +e
+    ./dnscrypt-proxy
+    set -e
+
+    popd
+}
+
+install_from_docker_installer_func()
+{
+    echoY "Installing dnscrypt-proxy to your ${INSTALL_ROOT_PATH}..."
+    sudo rm -rf ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+    mkdir -p ${INSTALL_ROOT_PATH}
+    docker run --rm -it -v ${INSTALL_ROOT_PATH}:/target rayruan/dnscrypt-proxy_${ARCH}:installer 
+    USER_NAME=$(id -un)
+    GROUP_NAME=$(id -gn)
+    #echo "### ${USER_NAME}"
+    sudo chown -hR ${USER_NAME}:${GROUP_NAME} ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+    cp dnsCryptSrc/public-resolvers.md* ${INSTALL_ROOT_PATH}/dnscrypt-proxy/
+
+    config_func
+    cp ./dnsCryptSrc/public* ${INSTALL_ROOT_PATH}/dnscrypt-proxy/
+}
+
+install_service_func()
+{
+	sudo sed -i '/^static domain_name_servers=.*/d' /etc/dhcpcd.conf
+	sudo sed -i '/^#static domain_name_servers=192.168.1.1$/a\static domain_name_servers=127.0.0.1' /etc/dhcpcd.conf
+
+    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+    sudo ./dnscrypt-proxy -service install
+    popd
+}
+
+uninstall_service_func()
+{
+    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+    sudo ./dnscrypt-proxy -service stop
+    sudo ./dnscrypt-proxy -service uninstall
+    popd
+
+	sudo sed -i '/^static domain_name_servers=.*/d' /etc/dhcpcd.conf
+}
+
 
 usage_func()
 {
     echo "./run.sh <cmd> <target>"
     echo ""
     echo "Supported cmd:"
-    echo "[ uninstall, check ]"
+    echo "[ check, uninstall, mk, install, uninstall ]"
     echo ""
     echo "Supported target:"
-    echo "[ sysDNS ]"
+    echo "[ sysDNS, cfgs, dns, service ]"
 }
 
 [ $# -lt 2 ] && echo "Invalid args count:$# " && usage_func && exit 1
 
 case $1 in
-    uninstall) echoY "Uninstalling $2..."
-        if [ $2 == "sysDNS" ] 
-        then
-            uninstall_systemDNS_func
-        else
-            echoR "Unknow target:$2, only support uninstalling targets [sysDNS]."
-            usage_func
-        fi
-        ;;
     check) echoY "Checking local $2 service ..."
         if [ $2 == "sysDNS" ] 
         then
@@ -61,6 +110,45 @@ case $1 in
             echoR "Unknow target:$2, only support checking targets [sysDNS]."
             usage_func
         fi
+        ;;
+    mk) echoY "Making configs of $2"
+        if [ $2 == "cfgs" ]
+        then
+            config_func
+        else
+            echoR "Unknow target:$2, only support mk targets [ cfgs ]."
+        fi
+        ;;
+    install) echoY "Installing..."
+        if [ $2 == "dns" ] 
+        then
+            #install_from_docker_installer_func
+            echoY "Unsupport install from docker installer now, please install relPkgs."
+        elif [ $2 == "service" ]
+        then
+            echoY "Installing dnscrypt-proxy service..."
+            install_service_func
+        else
+            echoR "Unknow target:$2, only support installing targets [dns, service]."
+        fi
+        echoG "Install $2 finished."
+        ;;
+    uninstall) echoY "Uninstalling..."
+        if [ $2 == "sysDNS" ] 
+        then
+            uninstall_systemDNS_func
+        elif [ $2 == "dns" ] 
+        then
+            echoY "Uinstalling dnscrypt-proxy service ..."
+            sudo rm -rf ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+        elif [ $2 == "service" ]
+        then
+            echoY "Installing dnscrypt-proxy service..."
+            uninstall_service_func
+        else
+            echoR "Unknow target:$2, only support uninstalling targets [dns, service]."
+        fi
+        echoG "Uninstall $2 finished."
         ;;
     *) echo "Unsupported cmd:$1."
         usage_func
