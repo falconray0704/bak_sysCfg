55e2f2fad4f70df9494bb2b429f469b370c1e93b
FalconRay <falconray@yahoo.com>
2019-12-10 15:14:39 +0800

    Reconstructed AP deploy script.

--- ap/pi/configure.sh
+++ ap/pi/configure.sh
@@ -38,25 +38,6 @@ ARCH=$(arch)
 RELEASE_ROOT_DIR="deployPkgs"
 INSTALL_ROOT_PATH=${HOME}/${RELEASE_ROOT_DIR}/${ARCH}
 
-rename_AP_out_func()
-{
-
-    sudo lshw -C network | grep -E "-network|description|logical name|serial"
-    echoY "Please input the name of device which use for AP out:"
-    read APOUT_NAME
-    APOUT_MAC=$(get_iether_MAC ${APOUT_NAME})
-    if [ ${APOUT_MAC} ]
-    then
-        echo "device mac: ${APOUT_MAC}"
-        cp ./cfgs/70-piAPOut_network_interfaces.rules ./${TMP_DIR}/
-        sed -i "s/macAddr/${APOUT_MAC}/" ./${TMP_DIR}/70-piAPOut_network_interfaces.rules
-        sudo cp ./${TMP_DIR}/70-piAPOut_network_interfaces.rules /etc/udev/rules.d/
-        cat ./${TMP_DIR}/70-piAPOut_network_interfaces.rules
-    else
-        echoR "Can not get the mac address of ${APOUT_NAME}"
-    fi
-}
-
 rename_AP_device_func()
 {
 
@@ -76,52 +57,23 @@ rename_AP_device_func()
     fi
 }
 
-get_args()
+rename_AP_out_func()
 {
-	#iw list
-	#lshw -C network
-#    sudo lshw -C network | grep -E "-network|description|logical name|serial"
-#    echoY "Please input the name of device which use for AP:"
-#    read APDEV_NAME
-    APDEV_MAC=$(get_iether_MAC ${APDEV_NAME})
-#    echoY "Please input the name of device which use for AP out:"
-#    read APOUT_NAME
-#    APOUT_MAC=$(get_iether_MAC ${APOUT_NAME})
 
-    echoC "MAC of ${APDEV_NAME} is: ${APDEV_MAC}"
-	echoY "Please input your AP channel number(eg:6):"
-	read apCh
-	echoY "Please input your AP SSID:"
-	read apSSID
-	echoY "Please input your AP password:"
-	read apPwd
-
-#	echoY "Please input your ss server IP:"
-#	read ssIP
-#	echoY "Please input your ss-redir local port:"
-#	read ssrListenPort
-
-
-	echo "Your AP name is: ${APDEV_NAME}"
-	echo "Your AP Mac address is: ${APDEV_MAC}"
-#	echo "Your AP out name is: ${APOUT_NAME}"
-	echo "Your AP out Mac address is: ${APOUT_MAC}"
-	echo "Your AP channel is: ${apCh}"
-	echo "Your AP SSID is: ${apSSID}"
-	echo "Your AP password is: ${apPwd}"
-
-#	echo "Your server IP is: ${ssIP}"
-#	echo "Your ss-redir local port is: ${ssrListenPort}"
-
-	echoY "Is it correct? [y/N]"
-	read isCorrect
-
-	if [ ${isCorrect}x = "Y"x ] || [ ${isCorrect}x = "y"x ]; then
-		echo "correct"
-	else
-		echo "incorrect"
-		exit 1
-	fi
+    sudo lshw -C network | grep -E "-network|description|logical name|serial"
+    echoY "Please input the name of device which use for AP out:"
+    read APOUT_NAME
+    APOUT_MAC=$(get_iether_MAC ${APOUT_NAME})
+    if [ ${APOUT_MAC} ]
+    then
+        echo "device mac: ${APOUT_MAC}"
+        cp ./cfgs/70-piAPOut_network_interfaces.rules ./${TMP_DIR}/
+        sed -i "s/macAddr/${APOUT_MAC}/" ./${TMP_DIR}/70-piAPOut_network_interfaces.rules
+        sudo cp ./${TMP_DIR}/70-piAPOut_network_interfaces.rules /etc/udev/rules.d/
+        cat ./${TMP_DIR}/70-piAPOut_network_interfaces.rules
+    else
+        echoR "Can not get the mac address of ${APOUT_NAME}"
+    fi
 }
 
 unmanaged_devices()
@@ -134,7 +86,42 @@ unmanaged_devices()
 
 }
 
-hostapd_config()
+get_hostapd_args_func()
+{
+	#iw list
+	#lshw -C network
+#    sudo lshw -C network | grep -E "-network|description|logical name|serial"
+#    echoY "Please input the name of device which use for AP:"
+#    read APDEV_NAME
+    APDEV_MAC=$(get_iether_MAC ${APDEV_NAME})
+
+    echoC "MAC of ${APDEV_NAME} is: ${APDEV_MAC}"
+	echoY "Please input your AP channel number(eg:6):"
+	read apCh
+	echoY "Please input your AP SSID:"
+	read apSSID
+	echoY "Please input your AP password:"
+	read apPwd
+
+
+	echo "Your AP name is: ${APDEV_NAME}"
+	echo "Your AP Mac address is: ${APDEV_MAC}"
+	echo "Your AP channel is: ${apCh}"
+	echo "Your AP SSID is: ${apSSID}"
+	echo "Your AP password is: ${apPwd}"
+
+	echoY "Is it correct? [y/N]"
+	read isCorrect
+
+	if [ ${isCorrect}x = "Y"x ] || [ ${isCorrect}x = "y"x ]; then
+		echo "correct"
+	else
+		echo "incorrect"
+		exit 1
+	fi
+}
+
+config_hostapd_func()
 {
 	#cmd="s/interface/interface=${apName}"
 	#echo "cmd:${cmd}"
@@ -153,7 +140,64 @@ hostapd_config()
 	#sudo cp ./${TMP_DIR}/hostapd.conf /etc/hostapd/
 }
 
-isc_DHCP_Server_config()
+config_AP_service_func()
+{
+    echo "Preparing systemd service config file for hostapd..."
+	cp cfgs/AP.service ./${TMP_DIR}/
+	sed -i "s/wlan0/${APDEV_NAME}/g" ./${TMP_DIR}/AP.service
+
+	echoC "=== after config ./${TMP_DIR}/AP.service start ==="
+	cat ./${TMP_DIR}/AP.service
+	echoC "=== after config ./${TMP_DIR}/AP.service end   ==="
+}
+
+cfg_AP_device_func()
+{
+    get_hostapd_args_func
+    unmanaged_devices
+    config_hostapd_func
+    config_AP_service_func
+}
+
+enable_AP_service_func()
+{
+	sudo systemctl daemon-reload
+	sudo systemctl enable AP.service
+}
+
+install_AP_device_func()
+{
+	sudo cp ./${TMP_DIR}/dhcpcd.conf /etc/dhcpcd.conf
+	sudo cp ./${TMP_DIR}/hostapd.conf /etc/hostapd/
+	sudo cp ./${TMP_DIR}/AP.service /lib/systemd/system/
+
+    enable_AP_service_func
+
+}
+
+disable_AP_service_func()
+{
+	sudo systemctl disable AP.service
+	sudo systemctl daemon-reload
+}
+
+release_AP_IP_func()
+{
+	sudo sed -i '/interface piAPDev' /etc/dhcpcd.conf
+	sudo sed -i '/static ip_address=192\.168\.11\.1\/24' /etc/dhcpcd.conf
+	sudo sed -i '/nohook wpa_supplicant' /etc/dhcpcd.conf
+}
+
+uninstall_AP_device_func()
+{
+    disable_AP_service_func
+	sudo rm /lib/systemd/system/AP.service 
+
+    release_AP_IP_func
+	sudo rm /etc/hostapd/hostapd.conf 
+}
+
+config_AP_DHCP_func()
 {
     echoY "Preparing config file for DHCP server for ${APDEV_NAME} AP node."
     cp ./cfgs/dhcpd.conf ./${TMP_DIR}/
@@ -166,15 +210,75 @@ isc_DHCP_Server_config()
 	echoC "=== after config ./${TMP_DIR}/isc-dhcp-server end   ==="
 }
 
-service_AP_config()
+enable_DHCP_service_func()
 {
-    echo "Preparing systemd service config file for hostapd..."
-	cp cfgs/AP.service ./${TMP_DIR}/
-	sed -i "s/wlan0/${APDEV_NAME}/g" ./${TMP_DIR}/AP.service
+    #sudo systemctl start isc-dhcp-server.service
+    sudo systemctl enable isc-dhcp-server.service
+}
 
-	echoC "=== after config ./${TMP_DIR}/AP.service start ==="
-	cat ./${TMP_DIR}/AP.service
-	echoC "=== after config ./${TMP_DIR}/AP.service end   ==="
+install_AP_DHCP_func()
+{
+    sudo cp ./${TMP_DIR}/dhcpd.conf /etc/dhcp/dhcpd.conf
+    sudo cp ./${TMP_DIR}/isc-dhcp-server /etc/default/
+
+    enable_DHCP_service_func
+}
+
+disable_DHCP_service_func()
+{
+    sudo systemctl stop isc-dhcp-server.service
+    sudo systemctl disable isc-dhcp-server.service
+}
+
+uninstall_AP_DHCP_func()
+{
+    disable_DHCP_service_func
+
+    sudo rm /etc/dhcp/dhcpd.conf
+    sudo rm /etc/default/
+}
+
+
+config_AP_DNS_func()
+{
+    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+	sed -i "s/^listen_addresses =.*/listen_addresses = \['127.0.0.1:53', '192.168.11.1:53'\]/" dnscrypt-proxy.toml
+    popd
+}
+
+install_AP_DNS_func()
+{
+    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+    sudo ./dnscrypt-proxy -service restart
+    popd
+}
+
+uninstall_AP_DNS_func()
+{
+    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
+	sed -i "s/^listen_addresses =.*/listen_addresses = \['127.0.0.1:53'\]/" dnscrypt-proxy.toml
+    sudo ./dnscrypt-proxy -service restart
+    popd
+}
+
+get_iptable_args_func()
+{
+    sudo lshw -C network | grep -E "-network|description|logical name|serial"
+    echoY "Please input the name of device which use for AP out:"
+
+    read APOUT_NAME
+    APOUT_MAC=$(get_iether_MAC ${APOUT_NAME})
+
+	echo "Your AP out name is: ${APOUT_NAME}"
+	echo "Your AP out Mac address is: ${APOUT_MAC}"
+
+	echoY "Please input your ss server IP:"
+	read ssIP
+	echoY "Please input your ss-redir local port:"
+	read ssrListenPort
+
+	echo "Your server IP is: ${ssIP}"
+	echo "Your ss-redir local port is: ${ssrListenPort}"
 }
 
 enableAP_ss_forward()
@@ -200,14 +304,14 @@ enableAP_ss_forward()
 
 ss_AP_forward_startup_config()
 {
-	sudo lshw -C network | grep -E "-network|description|logical name|serial"
+#	sudo lshw -C network | grep -E "-network|description|logical name|serial"
 
 	#echo "Please input your AP device Name:"
 	#read apName
 #	echo "Please input your output deviceName(eg:eth0):"
 #	read outInterface
 
-	echoY "All AP:${APDEV_NAME} packet will forward to: ${APOUT_NAME}"
+	echoY "All packets of ${APDEV_NAME} will be forward to: ${APOUT_NAME}"
 
 	echoY "Is it correct? [y/N]"
 	read isCorrect
@@ -229,75 +333,24 @@ ss_AP_forward_startup_config()
 
 }
 
-
-make_AP_configs_func()
+config_iptable_func()
 {
-#    mkdir -p ./${TMP_DIR}
-    get_args
-    unmanaged_devices
-    hostapd_config
-    isc_DHCP_Server_config
-    service_AP_config
-
-#    ss_AP_forward_startup_config
-}
-
-install_AP_configs_func()
-{
-
-	sudo cp ./${TMP_DIR}/dhcpcd.conf /etc/dhcpcd.conf
-    sudo cp ./${TMP_DIR}/dhcpd.conf /etc/dhcp/dhcpd.conf
-
-#    sudo cp ./${TMP_DIR}/dnscrypt-proxy.toml ~/dnsCryptProxy/
-
-	sudo cp ./${TMP_DIR}/hostapd.conf /etc/hostapd/
-	sudo cp ./${TMP_DIR}/AP.service /lib/systemd/system/
-
-#	sudo cp ./${TMP_DIR}/iptables.ipv4.nat /etc/iptables.ipv4.nat
-#    sudo cp ./cfgs/iptables /etc/network/if-up.d/
-
-    sudo cp ./${TMP_DIR}/isc-dhcp-server /etc/default/
-
-}
-
-enable_AP_service_func()
-{
-	sudo systemctl daemon-reload
-	sudo systemctl enable AP.service
-}
-
-enable_AP_dnscrypt_func()
-{
-    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
-	sed -i "s/^listen_addresses =.*/listen_addresses = \['127.0.0.1:53', '192.168.11.1:53'\]/" dnscrypt-proxy.toml
-    popd
+    get_iptable_args_func
+    ss_AP_forward_startup_config
 }
 
 
-disable_AP_service_func()
+install_iptable_func()
 {
-	sudo systemctl disable AP.service
-	sudo systemctl daemon-reload
+	sudo cp ./${TMP_DIR}/iptables.ipv4.nat /etc/iptables.ipv4.nat
+    sudo cp ./cfgs/iptables /etc/network/if-up.d/
+
 }
 
-disable_AP_dnscrypt_func()
+uninstall_iptable_func()
 {
-    pushd ${INSTALL_ROOT_PATH}/dnscrypt-proxy
-	sed -i "s/^listen_addresses =.*/listen_addresses = \['127.0.0.1:53'\]/" dnscrypt-proxy.toml
-    popd
-}
-
-
-enable_DHCP_service_func()
-{
-    #sudo systemctl start isc-dhcp-server.service
-    sudo systemctl enable isc-dhcp-server.service
-}
-
-disable_DHCP_service_func()
-{
-    sudo systemctl stop isc-dhcp-server.service
-    sudo systemctl disable isc-dhcp-server.service
+	sudo rm /etc/iptables.ipv4.nat
+    sudo rm /etc/network/if-up.d/iptables 
 }
 
 usage_func()
@@ -305,10 +358,10 @@ usage_func()
     echoY "./configure.sh <cmd> <target>"
     echo ""
     echoY "Supported cmd:"
-    echo "[ install, rename, mk, uninstall ]"
+    echo "[ install, rename, cfg, uninstall ]"
     echo ""
     echoY "Supported target:"
-    echo "[ dep, apDev, apCfgs, srvAP, srvDHCP, apOut ]"
+    echo "[ dep, devAP, devOut, apDHCP, apDNS, iptable ]"
 }
 
 
@@ -317,67 +370,82 @@ usage_func()
 mkdir -p ${TMP_DIR}
 
 case $1 in
-    install) echoY "Installing dependence..."
+    install) echoY "Installing AP dependence..."
         if [ $2 == "dep" ]
         then
             sudo apt-get -y install lshw hostapd isc-dhcp-server
-        elif [ $2 == "apCfgs" ]
+        elif [ $2 == "devAP" ]
         then
-            install_AP_configs_func
-        elif [ $2 == "srvAP" ]
+            install_AP_device_func
+
+            echoG "Install finished..."
+            echoY "Press any key to reboot system"
+            read rb
+            sudo reboot
+        elif [ $2 == "apDHCP" ]
         then
-            enable_AP_service_func
-            enable_AP_dnscrypt_func
-        elif [ $2 == "srvDHCP" ]
+            install_AP_DHCP_func
+        elif [ $2 == "apDNS" ]
         then
-            enable_DHCP_service_func
+            install_AP_DNS_func
+        elif [ $2 == "iptable" ]
+        then
+            install_iptable_func
         else
-            echoR "Command install only support targets [ dep, cfgs, srvAP, srvDHCP ]."
+            echoR "Command install only support targets [ dep, devAP, apDHCP, apDNS, iptable ]."
         fi
         ;;
     rename) echoY "Renaming AP device name to piAPDev..."
-        if [ $2 == "apDev" ]
+        if [ $2 == "devAP" ]
         then
             rename_AP_device_func
-        elif [ $2 == "apOut" ]
-	then
+        elif [ $2 == "devOut" ]
+        then
             rename_AP_out_func
         else
-            echoR "Command rename only support targets: [ apDev, apOut ]."
+            echoR "Command rename only support targets: [ devAP, devOut ]."
         fi
         ;;
-    mk) echoY "Making configs ..."
-        if [ $2 == "apCfgs" ]
+    cfg) echoY "Making configs for $2..."
+        if [ $2 == "devAP" ]
         then
-            make_AP_configs_func
+            cfg_AP_device_func
+        elif [ $2 == "apDHCP" ]
+        then
+            config_AP_DHCP_func
+        elif [ $2 == "apDNS" ]
+        then
+            config_AP_DNS_func
+        elif [ $2 == "iptable" ]
+        then
+            config_iptable_func
         else
-            echoR "Command mk only support targets [ cfgs ]."
+            echoR "Command cfg only support targets [ devAP, apDNS, iptable ]."
         fi
         ;;
     uninstall) echoY "Uninstalling $2 ..."
-        if [ $2 == "srvAP" ]
+        if [ $2 == "devAP" ]
         then
-            disable_AP_service_func
-#            disable_DHCP_service_func
-
-            disable_AP_dnscrypt_func
-            echoG "uninstall finished..."
+            uninstall_AP_device_func
+            echoG "Uninstall $2 finished..."
+            echoY "Press any key to reboot system"
+            read rb
+            sudo reboot
+        elif [ $2 == "apDHCP" ]
+        then
+            uninstall_AP_DHCP_func
+            echoG "uninstall $2 finished..."
             echoY "press any key to reboot system"
             read rb
-
             sudo reboot
-        elif [ $2 == "srvDHCP" ]
+        elif [ $2 == "apDNS" ]
         then
-#            disable_AP_service_func
-            disable_DHCP_service_func
-
-            echoG "uninstall finished..."
-            echoY "press any key to reboot system"
-            read rb
-
-            sudo reboot
+            uninstall_AP_DNS_func
+        elif [ $2 == "iptable" ]
+        then
+            uninstall_iptable_func
         else
-            echoR "Command uninstall only support targets [ srvAP, srvDHCP ]."
+            echoR "Command uninstall only support targets [ devAP, apDHCP, apDNS, iptable ]."
         fi
         ;;
     *) echo "Unsupported cmd:$1."
